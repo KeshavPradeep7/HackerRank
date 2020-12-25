67932df4a0faefe9a956d56bc7d44ba6cc5e1ef0
KeshavPradeep7 <keshavpradeepramanath@gmail.com>
2020-12-25 19:16:29 +0530

    Adding a commit in the test

--- HackerRank/src/com/keshavpradeep/GitTest.java
+++ HackerRank/src/com/keshavpradeep/GitTest.java
@@ -3,7 +3,7 @@ package com.keshavpradeep;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Git is wonderful");
+		System.out.println("Git is wonderful"); //This is sample
 	}
 
 }
