06fcddd44a0662d56c5d2a474e4cbd2858773f83
KeshavPradeep7 <keshavpradeepramanath@gmail.com>
2020-12-26 12:28:04 +0530

    Apple and Oranges

---
+++ HackerRank/src/com/keshavpradeep/ApplesAndOranges.java
@@ -0,0 +1,114 @@
+/*
+ * Sam's house has an apple tree and an orange tree that yield an abundance of fruit. Using the information given below, determine the number of apples and oranges that land on Sam's house.
+
+In the diagram below:
+
+The red region denotes the house, where  is the start point, and  is the endpoint. The apple tree is to the left of the house, and the orange tree is to its right.
+Assume the trees are located on a single point, where the apple tree is at point , and the orange tree is at point .
+When a fruit falls from its tree, it lands  units of distance from its tree of origin along the -axis. *A negative value of  means the fruit fell  units to the tree's left, and a positive value of  means it falls  units to the tree's right. *
+
+Function Description
+
+Complete the countApplesAndOranges function in the editor below. It should print the number of apples and oranges that land on Sam's house, each on a separate line.
+
+countApplesAndOranges has the following parameter(s):
+
+s: integer, starting point of Sam's house location.
+t: integer, ending location of Sam's house location.
+a: integer, location of the Apple tree.
+b: integer, location of the Orange tree.
+apples: integer array, distances at which each apple falls from the tree.
+oranges: integer array, distances at which each orange falls from the tree.
+Input Format
+
+The first line contains two space-separated integers denoting the respective values of  and .
+The second line contains two space-separated integers denoting the respective values of  and .
+The third line contains two space-separated integers denoting the respective values of  and .
+The fourth line contains  space-separated integers denoting the respective distances that each apple falls from point .
+The fifth line contains  space-separated integers denoting the respective distances that each orange falls from point .
+
+ */
+package com.keshavpradeep;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ApplesAndOranges {
+
+	// Complete the countApplesAndOranges function below.
+	static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        List<Integer> orangeList= new ArrayList<Integer>();
+        List<Integer> applList= new ArrayList<Integer>();
+        int appleCount=0;
+        int orangeCount=0;
+        for(int apple:apples){
+            applList.add(a+apple);
+        }
+        for(int orange:oranges){
+            orangeList.add(b+orange);
+        }
+        for(Integer i:applList){
+            if(i>=s&& i<=t){
+                appleCount++;
+            }
+        }
+        
+        for(Integer i:orangeList){
+            if(i>=s&& i<=t){
+                orangeCount++;
+            }
+        }
+        
+        System.out.println(appleCount);
+        System.out.println(orangeCount);
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] st = scanner.nextLine().split(" ");
+
+        int s = Integer.parseInt(st[0]);
+
+        int t = Integer.parseInt(st[1]);
+
+        String[] ab = scanner.nextLine().split(" ");
+
+        int a = Integer.parseInt(ab[0]);
+
+        int b = Integer.parseInt(ab[1]);
+
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        int[] apples = new int[m];
+
+        String[] applesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int applesItem = Integer.parseInt(applesItems[i]);
+            apples[i] = applesItem;
+        }
+
+        int[] oranges = new int[n];
+
+        String[] orangesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int orangesItem = Integer.parseInt(orangesItems[i]);
+            oranges[i] = orangesItem;
+        }
+
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+
+        scanner.close();
+    }
+   }
